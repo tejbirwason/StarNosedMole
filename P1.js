8c4999f9c697586b2e2213946e55723a1b6ba89a
Tejbir Wason <tejbirwason@gmail.com>
2016-01-31 20:06:47 -0800

    added helper rotation and multiply methods, added array version of tentacles, animated tentacles

--- P1.js
+++ P1.js
@@ -71,7 +71,7 @@ function makeCube() {
 
 // GEOMETRY
 var torsoGeometry = makeCube();
-var non_uniform_scale = new THREE.Matrix4().set(8,0,0,0, 0,8,0,0, 0,0,12,0, 0,0,0,1);
+var non_uniform_scale = new THREE.Matrix4().set(16,0,0,0, 0,16,0,0, 0,0,24,0, 0,0,0,1);
 torsoGeometry.applyMatrix(non_uniform_scale);
 
 // TO-DO: SPECIFY THE REST OF YOUR STAR-NOSE MOLE'S GEOMETRY. 
@@ -82,31 +82,79 @@ torsoGeometry.applyMatrix(non_uniform_scale);
 // Hint: Explicity declare new matrices using Matrix4().set     
 
 var headGeometry = makeCube();
-headGeometry.applyMatrix(new THREE.Matrix4().set(4,0,0,0, 0,4,0,0, 0,0,4,0, 0,0,0,1));
+headGeometry.applyMatrix(new THREE.Matrix4().set(8,0,0,0, 0,8,0,0, 0,0,8,0, 0,0,0,1));
 
 var noseGeometry = makeCube();
-noseGeometry.applyMatrix(new THREE.Matrix4().set(2,0,0,0, 0,2,0,0, 0,0,1.5,0, 0,0,0,1));
+noseGeometry.applyMatrix(new THREE.Matrix4().set(4,0,0,0, 0,4,0,0, 0,0,3,0, 0,0,0,1));
+
+var tailGeometry = makeCube();
+tailGeometry.applyMatrix(new THREE.Matrix4().set(2,0,0,0, 0,2,0,0, 0,0,12,0, 0,0,0,1));
+
+var lrgTentGeometry = makeCube();
+lrgTentGeometry.applyMatrix(new THREE.Matrix4().set(0.2,0,0,0, 0,0.2,0,0, 0,0,4,0, 0,0,0,1));
+
+var smallTentGeometry = makeCube();
+smallTentGeometry.applyMatrix(new THREE.Matrix4().set(0.1,0,0,0, 0,0.1,0,0, 0,0,3,0, 0,0,0,1));
 
 
 
 // MATRICES
-var torsoMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,4, 0,0,1,0, 0,0,0,1);
+function rot_x(angle){
+    return new THREE.Matrix4().set(1,0,0,0, 0,Math.cos(angle),-Math.sin(angle),0, 0,Math.sin(angle),Math.cos(angle),0, 0,0,0,1);
+}
+function rot_z(angle){
+    return new THREE.Matrix4().set(Math.cos(angle),-Math.sin(angle),0,0, Math.sin(angle),Math.cos(angle),0,0, 0,0,1,0, 0,0,0,1);
+}
+function rot_y(angle){
+    return new THREE.Matrix4().set(Math.cos(angle),0,Math.sin(angle),0, 0,1,0,0,  -Math.sin(angle),0,Math.cos(angle),0, 0,0,0,1);
+}
+function multiplyMatrices(a,b){
+    return new THREE.Matrix4().multiplyMatrices(a, b);
+}
+
+var torsoMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,8, 0,0,1,0, 0,0,0,1);
 
 // TO-DO: INITIALIZE THE REST OF YOUR MATRICES 
 // Note: Use of parent attribute is not allowed.
 // Hint: Keep hierarchies in mind!   
 // Hint: Play around with the headTorsoMatrix values, what changes in the render? Why?   
 
-var headMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,4, 0,0,1,8, 0,0,0,1);
-var headTorsoMatrix = new THREE.Matrix4();
-headTorsoMatrix.multiplyMatrices(torsoMatrix, headMatrix);
+var headMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,8, 0,0,1,16, 0,0,0,1);
+var headTorsoMatrix = multiplyMatrices(torsoMatrix, headMatrix);
 
-var noseMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,0, 0,0,1,2.5, 0,0,0,1);
-var noseHeadMatrix = new THREE.Matrix4();
-noseHeadMatrix.multiplyMatrices(headMatrix, noseMatrix);
-var noseFinalMatrix = new THREE.Matrix4();
-noseFinalMatrix.multiplyMatrices(torsoMatrix, noseHeadMatrix);
+// tail
+var tailMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-6, 0,0,1,-16, 0,0,0,1);
+var tailTorsoMatrix = multiplyMatrices(torsoMatrix, tailMatrix);
 
+// nose
+var noseMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,0, 0,0,1,5, 0,0,0,1);
+var noseFinalMatrix = multiplyMatrices(headTorsoMatrix, noseMatrix);
+
+// large tentacles
+var lRTM = [];
+var lRTMfinal = [];
+var lLTM = [];
+var lLTMfinal =[];
+
+for(var i=0;i<9;i++){
+  lRTM[i] = new THREE.Matrix4().set(1,0,0,2, 0,1,0,(i-4)/2.5, 0,0,1,3, 0,0,0,1);
+  lLTM[i] = new THREE.Matrix4().set(1,0,0,-2, 0,1,0,(i-4)/2.5, 0,0,1,3, 0,0,0,1);
+  lRTMfinal[i] = multiplyMatrices(noseFinalMatrix, lRTM[i]);
+  lLTMfinal[i] = multiplyMatrices(noseFinalMatrix, lLTM[i]);
+}
+
+// small tentacles
+var sRTM = [];
+var sRTMfinal = [];
+var sLTM = [];
+var sLTMfinal =[];
+
+for(var i=0;i<2;i++){
+  sRTM[i] = new THREE.Matrix4().set(1,0,0,1, 0,1,0,1-2*i, 0,0,1,3, 0,0,0,1);
+  sLTM[i] = new THREE.Matrix4().set(1,0,0,-1, 0,1,0,2*i-1, 0,0,1,3, 0,0,0,1);
+  sRTMfinal[i] = multiplyMatrices(noseFinalMatrix, sRTM[i]);
+  sLTMfinal[i] = multiplyMatrices(noseFinalMatrix, sLTM[i]);
+}
 
 
 // CREATE BODY
@@ -118,14 +166,50 @@ scene.add(torso);
 // Hint: Hint: Add one piece of geometry at a time, then implement the motion for that part. 
 //             Then you can make sure your hierarchy still works properly after each step.
 
+// create head
 var head = new THREE.Mesh(headGeometry,normalMaterial);
 head.setMatrix(headTorsoMatrix);
 scene.add(head);
 
+// create tail
+var tail = new THREE.Mesh(tailGeometry,normalMaterial);
+tail.setMatrix(tailTorsoMatrix);
+scene.add(tail);
+
+//create nose
 var nose = new THREE.Mesh(noseGeometry,normalMaterial);
 nose.setMatrix(noseFinalMatrix);
 scene.add(nose);
 
+//create large tentacles (array edition)
+var lRTmesh = [];
+var lLTmesh = [];
+for(var i=0;i<9;i++){
+  var lrgTentRight = new THREE.Mesh(lrgTentGeometry,normalMaterial);
+  lrgTentRight.setMatrix(lRTMfinal[i]);
+  scene.add(lrgTentRight);
+  lRTmesh[i] = lrgTentRight;
+
+  var lrgTentLeft = new THREE.Mesh(lrgTentGeometry,normalMaterial);
+  lrgTentLeft.setMatrix(lLTMfinal[i]);
+  scene.add(lrgTentLeft);
+  lLTmesh[i] = lrgTentLeft;
+}
+
+//create small tentacles (array edition)
+var sRTmesh = [];
+var sLTmesh = [];
+for(var i=0;i<2;i++){
+  var smallTentRight = new THREE.Mesh(smallTentGeometry,normalMaterial);
+  smallTentRight.setMatrix(sRTMfinal[i]);
+  scene.add(smallTentRight);
+  sRTmesh[i] = smallTentRight;
+
+  var smallTentLeft = new THREE.Mesh(smallTentGeometry,normalMaterial);
+  smallTentLeft.setMatrix(sLTMfinal[i]);
+  scene.add(smallTentLeft);
+  sLTmesh[i] = smallTentLeft;
+}
 
 
 // APPLY DIFFERENT JUMP CUTS/ANIMATIONS TO DIFFERNET KEYS
@@ -171,17 +255,28 @@ function updateBody() {
 
       p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
 
-      var rotateZ = new THREE.Matrix4().set(1,        0,         0,        0, 
-                                            0, Math.cos(-p),-Math.sin(-p), 0, 
-                                            0, Math.sin(-p), Math.cos(-p), 0,
-                                            0,        0,         0,        1);
-
-      var torsoRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,rotateZ);
+      var torsoRotMatrix = multiplyMatrices(torsoMatrix,rot_x(-p));
       torso.setMatrix(torsoRotMatrix); 
-      var headRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoRotMatrix,headMatrix);
+      var tailRotMatrix = multiplyMatrices(torsoRotMatrix,tailMatrix);
+      tail.setMatrix(tailRotMatrix);
+      var headRotMatrix = multiplyMatrices(torsoRotMatrix,headMatrix);
       head.setMatrix(headRotMatrix);
-      var noseRotMatrix = new THREE.Matrix4().multiplyMatrices(headRotMatrix,noseMatrix);
+      var noseRotMatrix = multiplyMatrices(headRotMatrix,noseMatrix);
       nose.setMatrix(noseRotMatrix);
+      for(var i=0;i<9;i++){
+        var lrgRightTentRotMatrix = multiplyMatrices(noseRotMatrix,lRTM[i]);
+        lRTmesh[i].setMatrix(lrgRightTentRotMatrix);
+        var lrgLeftTentRotMatrix = multiplyMatrices(noseRotMatrix,lLTM[i]);
+        lLTmesh[i].setMatrix(lrgLeftTentRotMatrix);
+      }
+      for(var i=0;i<2;i++){
+        var smallRightTentRotMatrix = multiplyMatrices(noseRotMatrix,sRTM[i]);
+        sRTmesh[i].setMatrix(smallRightTentRotMatrix);
+        var smallLeftTentRotMatrix = multiplyMatrices(noseRotMatrix,sLTM[i]);
+        sLTmesh[i].setMatrix(smallLeftTentRotMatrix);
+      }
+
+
       break
 
 
@@ -196,18 +291,73 @@ function updateBody() {
 
       p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
 
-      var rotateY = new THREE.Matrix4().set(Math.cos(-p),0, Math.sin(-p),  0, 
-                                            0,          1,        0,        0, 
-                                            -Math.sin(-p),0, Math.cos(-p), 0,
-                                            0,        0,         0,        1);
-
-      var headRotMatrix = new THREE.Matrix4().multiplyMatrices(headMatrix,rotateY);
-      var headFinalRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,headRotMatrix);
+      var headRotMatrix = multiplyMatrices(headMatrix,rot_y(-p));
+      var headFinalRotMatrix = multiplyMatrices(torsoMatrix,headRotMatrix);
       head.setMatrix(headFinalRotMatrix); 
-      var noseRotMatrix = new THREE.Matrix4().multiplyMatrices(headFinalRotMatrix,noseMatrix);
+      var noseRotMatrix = multiplyMatrices(headFinalRotMatrix,noseMatrix);
       nose.setMatrix(noseRotMatrix);
+      for(var i=0;i<9;i++){
+        var lrgRightTentRotMatrix = multiplyMatrices(noseRotMatrix,lRTM[i]);
+        lRTmesh[i].setMatrix(lrgRightTentRotMatrix);
+        var lrgLeftTentRotMatrix = multiplyMatrices(noseRotMatrix,lLTM[i]);
+        lLTmesh[i].setMatrix(lrgLeftTentRotMatrix);
+      }
+      for(var i=0;i<2;i++){
+        var smallRightTentRotMatrix = multiplyMatrices(noseRotMatrix,sRTM[i]);
+        sRTmesh[i].setMatrix(smallRightTentRotMatrix);
+        var smallLeftTentRotMatrix = multiplyMatrices(noseRotMatrix,sLTM[i]);
+        sLTmesh[i].setMatrix(smallLeftTentRotMatrix);
+      }
       break
 
+
+      case((key == "T" || key == "V" ) && animate):
+      var time = clock.getElapsedTime(); // t seconds passed since the clock started.
+
+      if (time > time_end){
+        p = p1;
+        animate = false;
+        break;
+      }
+
+      p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
+
+      var tailRotMatrix = multiplyMatrices(tailMatrix,rot_y(-p));
+      var tailFinalRotMatrix = multiplyMatrices(torsoMatrix,tailRotMatrix);
+      tail.setMatrix(tailFinalRotMatrix); 
+      break
+
+      case((key == "N" ) && animate):
+      var time = clock.getElapsedTime(); // t seconds passed since the clock started.
+
+      if (time > time_end){
+        p = p1;
+        animate = false;
+        break;
+      }
+
+      p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
+
+      for(var i=0;i<9;i++){
+        var lrgTentRightRotMatrix = multiplyMatrices(lRTM[i],rot_y(p));
+        var lrgTentRightFinalRotMatrix = multiplyMatrices(noseFinalMatrix,lrgTentRightRotMatrix);
+        lRTmesh[i].setMatrix(lrgTentRightFinalRotMatrix); 
+
+        var lrgTentLeftRotMatrix = multiplyMatrices(lLTM[i],rot_y(-p));
+        var lrgTentLeftFinalRotMatrix = multiplyMatrices(noseFinalMatrix,lrgTentLeftRotMatrix);
+        lLTmesh[i].setMatrix(lrgTentLeftFinalRotMatrix);
+      }
+
+      for(var i=0;i<2;i++){
+        var smallTentRightRotMatrix = multiplyMatrices(sRTM[i],rot_y(p));
+        var smallTentRightFinalRotMatrix = multiplyMatrices(noseFinalMatrix,smallTentRightRotMatrix);
+        sRTmesh[i].setMatrix(smallTentRightFinalRotMatrix); 
+
+        var smallTentLeftRotMatrix = multiplyMatrices(sLTM[i],rot_y(-p));
+        var smallTentLeftFinalRotMatrix = multiplyMatrices(noseFinalMatrix,smallTentLeftRotMatrix);
+        sLTmesh[i].setMatrix(smallTentLeftFinalRotMatrix);
+      }
+      break
       // TO-DO: IMPLEMENT JUMPCUT/ANIMATION FOR EACH KEY!
       // Note: Remember spacebar sets jumpcut/animate!
       
@@ -237,11 +387,15 @@ keyboard.domElement.addEventListener('keydown',function(event){
   else if(keyboard.eventMatches(event,"E")){ 
     (key == "E")? init_animation(p1,p0,time_length) : (init_animation(0,-Math.PI/4,1), key = "E")} 
   else if(keyboard.eventMatches(event,"H")){ 
-    (key == "H")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/4,1), key = "H")}   
+    (key == "H")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/5,1), key = "H")}   
   else if(keyboard.eventMatches(event,"G")){ 
-    (key == "G")? init_animation(p1,p0,time_length) : (init_animation(0,-Math.PI/4,1), key = "G")} 
-
-
+    (key == "G")? init_animation(p1,p0,time_length) : (init_animation(0,-Math.PI/5,1), key = "G")} 
+  else if(keyboard.eventMatches(event,"V")){ 
+    (key == "V")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/5,1), key = "V")} 
+  else if(keyboard.eventMatches(event,"T")){ 
+    (key == "T")? init_animation(p1,p0,time_length) : (init_animation(0,-Math.PI/5,1), key = "T")} 
+  else if(keyboard.eventMatches(event,"N")){ 
+    (key == "N")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/5,1), key = "N")} 
   // TO-DO: BIND KEYS TO YOUR JUMP CUTS AND ANIMATIONS
   // Note: Remember spacebar sets jumpcut/animate! 
   // Hint: Look up "threex.keyboardstate by Jerome Tienne" for more info.
